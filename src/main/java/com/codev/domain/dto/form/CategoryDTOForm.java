c0948fae97b0ddf2892bd677fba6b7b333a6b18e
brenooduarte <brenoo.duarte@hotmail.com>
2023-08-11 16:07:44 -0300

    feat: implement validation in category

--- src/main/java/com/codev/domain/dto/form/CategoryDTOForm.java
+++ src/main/java/com/codev/domain/dto/form/CategoryDTOForm.java
@@ -1,10 +1,12 @@
 package com.codev.domain.dto.form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CategoryDTOForm {
 
+    @NotBlank
     private String name;
 
 }
