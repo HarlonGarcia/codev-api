eeedb4726dfcf5da6725ebd9114e6b966e5e732c
Harlon Garcia <harloongarcia@gmail.com>
2024-10-17 23:50:37 -0300

    Enhanced NullAwareBeanUtils helper copyProperties method

--- src/main/java/com/codev/utils/helpers/NullAwareBeanUtilsBean.java
+++ src/main/java/com/codev/utils/helpers/NullAwareBeanUtilsBean.java
@@ -1,14 +1,17 @@
 package com.codev.utils.helpers;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.PropertyUtilsBean;
 
 public class NullAwareBeanUtilsBean extends BeanUtilsBean {
-
-    @Override
-    public void copyProperties(Object dest, Object orig) throws IllegalAccessException, InvocationTargetException {
+    public void copyProperties(
+        Object dest,
+        Object orig,
+        String... ignoreProperties
+    ) throws IllegalAccessException, InvocationTargetException {
         if (orig == null) {
             return;
         }
@@ -19,7 +22,9 @@ public class NullAwareBeanUtilsBean extends BeanUtilsBean {
             for (String property : propertyUtils.describe(orig).keySet()) {
                 try {
                     Object value = propertyUtils.getProperty(orig, property);
-                    if (value != null) {
+                    boolean hasToIgnore = Arrays.asList(ignoreProperties).contains(property);
+
+                    if (value != null && !hasToIgnore) {
                         propertyUtils.setProperty(dest, property, value);
                     }
                 } catch (NoSuchMethodException e) {
