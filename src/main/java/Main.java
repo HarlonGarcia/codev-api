ba3790f1e44c886ce35173908f82fcd7ac382d2c
Breno Duarte <brenoo.duarte@hotmail.com>
2024-11-27 12:38:40 -0300

    refactor: compose.yaml and application.properties changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import io.quarkus.runtime.Quarkus;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1 " + System.getProperty("quarkus.datasource.jdbc.url"));
+        System.out.println("2 " + System.getProperty("%prod.quarkus.datasource.jdbc.url"));
+        System.out.println("3 " + System.getProperty("prod.quarkus.datasource.jdbc.url"));
         Quarkus.run(args);
     }
 }
